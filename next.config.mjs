bea1d11482f86ec78587ab39fad86d190b5c44be
ozkite <oscarx22@gmail.com>
2026-01-02 13:13:08 +0800

    Fix webpack config for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     webpack: (config) => {
         config.externals.push('pino-pretty', 'lokijs', 'encoding')
+        config.resolve.fallback = { fs: false, net: false, tls: false };
         return config
     },
     eslint: {
